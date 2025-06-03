ac0a866419d739d1399309f5f05baa1637a849f6
Rahul <87422105+Rahul-2006@users.noreply.github.com>
2025-06-03 22:47:10 +0530

    Update sql_connector.py

--- backend/sql_connector.py
+++ backend/sql_connector.py
@@ -13,5 +13,7 @@ def get_sql_connection():
             host=os.getenv("DB_HOST"),
             user=os.getenv("DB_USER"),
             password=os.getenv("DB_PASSWORD"),
-            database=os.getenv("DB_NAME"))
+            database=os.getenv("DB_NAME"),
+            ssl_disabled=False
+        )
         return __cnx
